0904d7c199bf8715fd9c186ecb6da79cca1a2727
Eldar Badalbeyli <eld.bad@yandex.com>
2024-10-02 17:36:17 +0500

    Naming convention fix

--- internal/entity/task.go
+++ internal/entity/task.go
@@ -3,9 +3,9 @@ package entity
 import "time"
 
 type Task struct {
-	Id            int
-	Name          string
-	Description   string
-	Creation_date time.Time
-	Status        bool
+	Id           int
+	Name         string
+	Description  string
+	CreationDate time.Time
+	Status       bool
 }
