b22a45c42f996a19536d681585ee6ec9450cfc48
Eldar Badalbeyli <eld.bad@yandex.com>
2024-10-08 19:26:33 +0500

    Task logic injection

--- cmd/todolist-web/main.go
+++ cmd/todolist-web/main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"github.com/eldbad/todolist-web/internal/handlers"
+	"github.com/eldbad/todolist-web/internal/usecase"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	th := handlers.TaskHandler{}
 
-	r.GET("/get_tasks", th.GetTask)
+	tl := usecase.TaskUsecase{}
+	th := handlers.NewTaskHandler(tl)
+	th.Register(&r.RouterGroup)
+
 	r.Run()
 
-	log.Error().Stack().Err(err).Msg("")
+	// log.Error().Stack().Err(err).Msg("")
 }
--- internal/usecase/task_logic.go
+++ internal/usecase/task_logic.go
@@ -1,7 +1,11 @@
 package usecase
 
+import (
+	"github.com/eldbad/todolist-web/internal/entity"
+)
+
 type TaskUsecase struct{}
 
-func (tl *TaskUsecase) GetAllTasks() {
-
+func (tl TaskUsecase) GetAllTasks() ([]entity.Task, error) {
+	panic("not implemented")
 }
