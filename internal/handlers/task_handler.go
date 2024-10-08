119491ef438cc1793308b6ae6889e11d85b9d3dd
Eldar Badalbeyli <eld.bad@yandex.com>
2024-10-08 19:29:38 +0500

    Naming fix and taskHandler became private

--- internal/handlers/task_handler.go
+++ internal/handlers/task_handler.go
@@ -8,26 +8,26 @@ import (
 	"github.com/eldbad/todolist-web/internal/entity"
 )
 
-type TaskHandler struct {
-	taskLogic taskUsecase
+type taskHandler struct {
+	taskUsecase taskUsecase
 }
 
 type taskUsecase interface {
 	GetAllTasks() ([]entity.Task, error)
 }
 
-func NewTaskHandler(tl taskUsecase) *TaskHandler {
-	return &TaskHandler{
-		taskLogic: tl,
+func NewTaskHandler(tl taskUsecase) *taskHandler {
+	return &taskHandler{
+		taskUsecase: tl,
 	}
 }
 
-func (th TaskHandler) Register(r *gin.RouterGroup) {
+func (th taskHandler) Register(r *gin.RouterGroup) {
 	r.GET("/get_task", th.GetTask) // TODO: Make it the same with other methods
 }
 
-func (th *TaskHandler) GetTask(c *gin.Context) {
-	tasks, err := th.taskLogic.GetAllTasks()
+func (th *taskHandler) GetTask(c *gin.Context) {
+	tasks, err := th.taskUsecase.GetAllTasks()
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{})
 	}
@@ -35,18 +35,18 @@ func (th *TaskHandler) GetTask(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"tasks": tasks})
 }
 
-func (th *TaskHandler) GetTasks(c *gin.Context) {
+func (th *taskHandler) GetTasks(c *gin.Context) {
 	panic("not implemented")
 }
 
-func (th *TaskHandler) CreateTask(c *gin.Context) {
+func (th *taskHandler) CreateTask(c *gin.Context) {
 	panic("not implemented")
 }
 
-func (th *TaskHandler) UpdateTask(c *gin.Context) {
+func (th *taskHandler) UpdateTask(c *gin.Context) {
 	panic("not implemented")
 }
 
-func (th *TaskHandler) DeleteTask(c *gin.Context) {
+func (th *taskHandler) DeleteTask(c *gin.Context) {
 	panic("not implemented")
 }
