11d25bb47dca862dc85fe46a369d6f2f82ae78d1
Eldar Badalbeyli <eld.bad@yandex.com>
2024-10-11 22:51:01 +0500

    Fixed db variable in repository

--- internal/repository/task_repository.go
+++ internal/repository/task_repository.go
@@ -14,8 +14,7 @@ type TaskRepository struct {
 }
 
 func NewTaskRepository(db *bun.DB) *TaskRepository {
-	postgresql.InitDB(dsn)
-	tr := &TaskRepository{}
+	tr := &TaskRepository{db: db}
 
 	return tr
 }
@@ -24,7 +23,7 @@ func (tr *TaskRepository) FindAll() ([]entity.Task, error) {
 	tasks := []entity.Task{}
 	ctx := context.Background()
 
-	err := postgresql.DB.NewSelect().Model(&tasks).Scan(ctx)
+	err := tr.db.NewSelect().Model(&tasks).Scan(ctx)
 	if err != nil {
 		// TODO: log an error
 		logging.Logger.Error().Msg("oops from zerolog")
@@ -36,14 +35,14 @@ func (tr *TaskRepository) FindAll() ([]entity.Task, error) {
 func (tr *TaskRepository) FindById(id int) (entity.Task, error) {
 	task := entity.Task{}
 	ctx := context.Background()
-	err := postgresql.DB.NewSelect().Model(&task).Where("id = ?", id).Scan(ctx)
+	err := tr.db.NewSelect().Model(&task).Where("id = ?", id).Scan(ctx)
 
 	return task, err
 }
 
 func (tr *TaskRepository) Create(task *entity.Task) error {
 	ctx := context.Background()
-	_, err := postgresql.DB.NewInsert().Model(task).Exec(ctx)
+	_, err := tr.db.NewInsert().Model(task).Exec(ctx)
 
 	return err
 }
@@ -52,7 +51,7 @@ func (tr *TaskRepository) Update(id int) error {
 	task := entity.Task{Id: id}
 	ctx := context.Background()
 
-	_, err := postgresql.DB.NewUpdate().Model(task).WherePK().Exec(ctx)
+	_, err := tr.db.NewUpdate().Model(task).WherePK().Exec(ctx)
 
 	return err
 }
@@ -61,7 +60,7 @@ func (tr *TaskRepository) Delete(id int) error {
 	task := entity.Task{Id: id}
 	ctx := context.Background()
 
-	_, err := postgresql.DB.NewDelete().Model(task).WherePK().Exec(ctx)
+	_, err := tr.db.NewDelete().Model(task).WherePK().Exec(ctx)
 
 	return err
 }
